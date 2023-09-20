e20cf478e679eb9f128dc342765e5673af8bf7d8
newGameDK <hr.storm@gmail.com>
2023-09-20 12:08:41 +0200

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
 let stepCount = 0
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
-    
+input.onGesture(Gesture.Shake, function () {
     stepCount = stepCount + 1
     basic.showNumber(stepCount)
 })
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onButtonPressed(Button.A, function () {
     stepCount = 0
     basic.showNumber(stepCount)
 })
